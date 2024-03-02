db3ae63a78f3fea5d5eea13e351f85a53aa4f389
Agajan Sahatov <agajan.st@gmail.com>
2024-03-02 18:50:37 +0800

    Handle HTTP Delete Requests

--- app.js
+++ app.js
@@ -70,7 +70,18 @@ const validateCourse = (course) => {
     return schema.validate(course);
 };
 
+app.delete("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) { 
+        res.status(404).send("The course with the given ID was not found!");
+        return;
+    }
 
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
 
 
 
